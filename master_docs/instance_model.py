3887ff2d7da5afc5a428bff8c38b177cc1da088b
Emil Larsson <emil.larsson@nordlo.com>
2020-09-12 11:15:03 +0200

    Added delete class

--- master_docs/instance_model.py
+++ master_docs/instance_model.py
@@ -3,7 +3,7 @@ from ipaddress import IPv4Address, IPv4Network
 from pydantic import Json, UUID4
 
 
-class Instance:
+class CreateInstance:
     """
         This class describes what information and type is needed to construct this object.
     """
@@ -13,3 +13,10 @@ class Instance:
     root_disk_size: int = 0  # 20480 (MB)
     network: IPv4Network = IPv4Network(address="192.168.0.1/24")  # 192.168.0.1/24
     ip_address: IPv4Address = IPv4Address(address="192.168.0.10")  # 192.168.0.10
+
+
+class DeleteInstance:
+    """
+        This class describes what information and type is needed to delete this object.
+    """
+    uuid: UUID4 = UUID4()  # e2f4dfbc-0994-4db6-a5bf-e40a86a0890b
